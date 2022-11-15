7498cb5c04f206719f3a2612993e4e457a36f5c2
BryanBac <49624724+BryanBac@users.noreply.github.com>
2022-11-15 13:44:26 -0600

    Grafica csv
    graficacion de csv

--- R/Examen.R
+++ R/Examen.R
@@ -1,5 +1,9 @@
+# Obtener datos y crear dataframe a travez de csv
+
 install.packages("readr")
 library(readr)
 top <- "C:\\Users\\HP15DA0023LA\\Documents\\GitHub\\Mysql-R\\data.csv"
 top_csv <- read.csv(top)
 
+#  hora de la graficada
+plot(top_csv)
