2a8c2549efb32e4f10727b81a5d260ca8465b770
BryanBac <49624724+BryanBac@users.noreply.github.com>
2022-11-15 13:25:31 -0600

    lectura de csv en r
    se corrigió la creación de archivo csv, y se creó el dataframe

---
+++ R/Examen.R
@@ -0,0 +1,5 @@
+install.packages("readr")
+library(readr)
+top <- "C:\\Users\\HP15DA0023LA\\Documents\\GitHub\\Mysql-R\\data.csv"
+top_csv <- read.csv(top)
+
--- main.py
+++ main.py
@@ -4,4 +4,4 @@ from Database.Conexion import Conexion
 conec: Conexion = Conexion()
 conec.conectar()
 dataframe = conec.obtener_tabla()
-dataframe.to_csv("data", index=False)
+dataframe.to_csv("data.csv", index=False)
